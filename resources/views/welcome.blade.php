231be11879445ecf970e2887f054b6777a60c609
Darko <c.darko3@gmail.com>
2018-02-03 16:44:50 +0100

    clmtrackr imported

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -78,7 +78,7 @@
             @endif
 
             <div class="content">
-                <h1>Wellcome </h1>
+                <h1>Welcome </h1>
             </div>
         </div>
     </body>
